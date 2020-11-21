090595a6b0e15583d197336312e6369455ece1bc
smileyuichi <smile.yuichi.1013@gmail.com>
2020-11-21 23:38:03 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <div><p>Hello world!</p></div>;
+  return (
+    <div>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => console.log("I'm clicked.")} />;
+    </div>
+  )
 }
 
 export default App;

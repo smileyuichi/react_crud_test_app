085e6b1970c7518a148072b32fcae6ad1b5fc54f
smileyuichi <smile.yuichi.1013@gmail.com>
2020-11-22 17:47:54 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-function App() {
+
+// function App() {
+//   return (
+//     <div>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onChange={() => console.log("I'm clicked.")} />;
+//     </div>
+//   )
+// }
+
+const App = () => {
   return (
     <div>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => console.log("I'm clicked.")} />;
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
     </div>
   )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;

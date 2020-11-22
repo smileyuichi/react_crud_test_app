c8f34ed28b3992c2636953c4358ad2dbd807af6d
smileyuichi <smile.yuichi.1013@gmail.com>
2020-11-22 22:40:51 +0900

    fix error ane add yarn react-is

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import './index.css';
-import reducer from './reducers';
+import reducer from './reducers'
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
@@ -13,12 +13,10 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
     <App />
-    </Provider>,
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux'
 import count from './count'
 
-
 export default combineReducers({count})

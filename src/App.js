3b1efa9da05f747c0e2b2b840d0de9e8ffab9d1c
smileyuichi <smile.yuichi.1013@gmail.com>
2020-11-22 18:25:17 +0900

    learn prop-types

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-
-// function App() {
-//   return (
-//     <div>
-//       <label htmlFor="bar">bar</label>
-//       <input type="text" onChange={() => console.log("I'm clicked.")} />;
-//     </div>
-//   )
-// }
+import PropTypes from 'prop-types';
 
 const App = () => {
   const profiles = [
     {name: "Taro",age:10},
-    {name: "Hanako",age:8},
-    {name: "Yuichi"},
+    {name: "Hanako",age:12},
+    // {name: "Yuichi"},
   ]
   return (
     <div>
@@ -30,8 +22,9 @@ const User = (props) => {
   return <div>Hi, I am {props.name},and { props.age} years old! </div>
 }
 
-User.defaultProps = {
-  age: 1
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
 export default App;
